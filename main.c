26964f15022883b2002cf50ecf79510c1e418769
Jinq jia Lim <jinlim@u81z05s02.42kl.edu.my>
2021-10-21 18:58:57 +0800

    created file stat and unlink functions

--- main.c
+++ main.c
@@ -7,6 +7,12 @@
 #include <unistd.h>
 #include <limits.h>
 
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <fcntl.h>
+
 void	ft_readline()
 {
 	char	*inpt;
@@ -40,10 +46,67 @@ void	ft_changepath()
 	chdir("..");
 }
 
+void	ft_stat()
+{
+	//https://stackoverflow.com/questions/3138600/correct-use-of-stat-on-c
+	char *file = "myfile.txt";
+	int	dev;
+	int ino;
+	int uid;
+	int size;
+
+	struct stat *buf = malloc(sizeof(struct stat));
+	if (stat(file, buf) == 0)
+	{
+		dev = buf->st_dev; //use man to know that structs are there
+		ino = buf->st_ino;
+		uid = buf->st_uid;
+		size = buf->st_size;
+		printf("\"%s\" info:\n", file);
+		printf("\tDevide ID:\t %d.\n", dev);
+		printf("\tInode Number:\t %d.\n", ino);
+		printf("\tUser ID:\t %d.\n", uid);
+		printf("\tSize:\t\t %d bytes.\n", size);
+	}
+	else
+		perror(file);
+	free(buf);
+	//lstat used to describe information of a symbolic link
+}
+
+void	ft_fstat()
+{
+	int		fd;
+	char	*file = "myfile.txt";
+	int		size;
+
+	struct stat *buf = malloc(sizeof(struct stat));
+	fd = open("myfile.txt", O_RDONLY);
+	if (stat(file, buf) == 0)
+	{
+		size = buf->st_size;
+		printf("\"%s\" info:\n", file);
+		printf("\tSize:\t\t %d bytes.\n", size);
+	}
+	else
+		perror(file);
+	free(buf);
+	close(fd);
+}
+
+void	ft_unlink()
+{
+	char	*file = "myfile.txt";
+	unlink(file); //deletes file
+}
+
 int	main()
 {
 	// ft_readline();
 	ft_getpath();
-	ft_changepath();
-	ft_getpath();
+	// ft_changepath();
+	// ft_getpath();
+	ft_stat();
+	ft_fstat();
+	// ft_unlink();
 }
