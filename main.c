a15fd39e1edc553b89c8dc89190d2f2b24e0f788
Jinq jia Lim <jinlim@u81z05s01.42kl.edu.my>
2021-10-26 16:42:28 +0800

    created ft_pipe but has issue

--- main.c
+++ main.c
@@ -145,17 +145,65 @@ void	ft_execve()
 	printf("Error\n");
 }
 
+void	ft_pipe()
+{
+	int		pipefd1[2]; //C => P
+	int		pipefd2[2]; //P => C
+	pid_t	cpid;
+	int		x;
+	int		y;
+
+	if (pipe(pipefd1) != 0)
+		perror("pipe1");
+	if (pipe(pipefd2) != 0)
+		perror("pipe2");
+	cpid = fork();
+	if (cpid == -1)
+		perror("fork");
+	if (cpid == 0) //child process
+	{
+		close(pipefd1[0]); //close unused C => P read end
+		close(pipefd2[1]); //close unused P => C write end
+		// x = readline("Enter text: ");
+		scanf("Child num input: %d", &x);
+		if (write(pipefd1[1], &x, 10) == -1)
+			perror("child write");
+		printf("Child wrote: %d\n", x);
+		if (read(pipefd2[0], &x, 10) == -1)
+			perror("child read");
+		printf("Child read: %d\n", x);
+		close(pipefd1[1]);
+		close(pipefd2[0]);
+	}
+	else //parent process
+	{
+		close(pipefd1[1]);
+		close(pipefd2[0]);
+		scanf("Parent num input: %d", &y);
+		if (write(pipefd2[1], &y, 10) == -1)
+			perror("parent write");
+		printf("Parent write: %d\n", y);
+		if (read(pipefd1[0], &y, 10) == -1)
+			perror("parent read");
+		printf("Parent read: %d\n", y);
+		close(pipefd1[0]); //child reads from pipe
+		close(pipefd2[1]);
+		wait(NULL);
+	}
+}
+
 int	main()
 {
 	signal(SIGINT, ft_sigint);	//ctrl - C
 	signal(SIGQUIT, ft_sigquit); //ctrl - \ /
 	// ft_tty();
 	// ft_readline();
-	ft_getpath();
+	// ft_getpath();
 	// ft_changepath();
 	// ft_getpath();
 	// ft_stat();
 	// ft_fstat();
 	// ft_unlink();
 	// ft_execve();
+	ft_pipe(); //got issues with 2 way communication
 }
