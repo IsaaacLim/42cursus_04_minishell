37c423a30645e11eaef4fbddc8608f9d01424f74
Jinq jia Lim <jinlim@u81z01s05.42kl.edu.my>
2021-11-04 15:57:59 +0800

    modified for env command

--- testing-isaac/hello.c
+++ testing-isaac/hello.c
@@ -4,9 +4,15 @@ int main(int argc, char *argv[], char *envp[])
 {
     int i;
 
-    printf("File name: %s\n", argv[0]);
-    i = 0;
-    while (++i < 5)
-       printf("%s ", argv[i]);
-    printf("\n");
+    // printf("File name: %s\n", argv[0]);
+    // i = 0;
+    // while (++i < 5)
+    //    printf("%s ", argv[i]);
+    int x = 0;
+    while (envp[x])
+    {
+        printf("%s\n", envp[x]);
+        x++;
+    }
+    // printf("\n");
 }
