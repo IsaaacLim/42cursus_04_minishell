63bb34533623286892402102b92b003f82630a95
Jinq jia Lim <jinlim@u81z01s05.42kl.edu.my>
2021-11-04 16:30:08 +0800

    created pwd

---
+++ srcs/built_ins/pwd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+
+int	main(int argc, char *argv[])
+{
+	char cwd[PATH_MAX];
+
+	if (getcwd(cwd, sizeof(cwd)) != NULL)
+		printf("%s\n", cwd);
+	else
+		perror("pwd error");
+}
