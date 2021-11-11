0172c413460063c3fea4fc5bd2387632f7c67bd5
jiaaren <jiaren.kjr@gmail.com>
2021-11-11 12:07:20 +0800

    2nd draft version

--- srcs/built_ins/export.c
+++ srcs/built_ins/export.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <limits.h>
 
 /*
 	TO handle?
@@ -124,13 +125,145 @@ void env_command(t_list *env)
 	while (env)
 	{
 		envar = (t_envar *)env->content;
-		printf("%s", envar->name);
 		if (envar->set)
-		{
-			printf("=%s\n", envar->word);
-		}
+			printf("%s=%s\n", envar->name, envar->word);
 		env = env->next;
 	}
+}
+
+int ft_lstlen(t_list *lst)
+{
+	int len;
+
+	len = 0;
+	while (lst)
+	{
+		len++;
+		lst = lst->next;
+	}
+	return (len);
+}
+
+// do i even need to do this?
+void sort_print(t_envar ***env, int len)
+{
+	int i;
+	int j;
+	t_envar **ptr;
+	t_envar *tmp;
+
+	ptr = *env;
+	i = 0;
+	while (i < len)
+	{
+		j = i + 1;
+		while (j < len)
+		{
+			if (ft_strncmp(ptr[i]->name, ptr[j]->name, __INT_MAX__) > 0)
+			{
+				tmp = ptr[i];
+				ptr[i] = ptr[j];
+				ptr[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+void export_command(t_list *env)
+{
+	int len;
+	t_envar **tmp_arr;
+	int i;
+
+	len = ft_lstlen(env);
+	tmp_arr = malloc(sizeof(t_envar *) * len);
+	i = 0;
+	while (env)
+	{
+		tmp_arr[i++] = (env)->content;
+		env = env->next;
+	}
+	// SORT
+	sort_print(&tmp_arr, len);
+	// PRINT
+	i = 0;
+	// to exclude environment variable _
+	while (i < len - 1)
+	{
+		printf("declare -x %s", tmp_arr[i]->name);
+		if (tmp_arr[i]->set)
+			printf("=\"%s\"", tmp_arr[i]->word);
+		printf("\n");
+		i++;
+	}
+	free(tmp_arr);
+}
+
+int error = false;
+
+t_list *found_env(t_list **env, t_list **prev, char *env_str)
+{
+	t_list *tmp_lst;
+	t_envar *tmp_env;
+
+	tmp_lst = *env;
+	while (tmp_lst)
+	{
+		// need to typecast that's why pretty confusing
+		tmp_env = (t_envar *)tmp_lst->content;
+		if (ft_strncmp(tmp_env->name, env_str, __INT_MAX__) == 0)
+			return (tmp_lst);
+		if (prev)
+			*prev = tmp_lst;
+		tmp_lst = tmp_lst->next;
+	}
+	return (NULL);
+}
+
+void export_add(t_list **env, char *env_str)
+{
+	t_envar *parse_env;
+	t_list *found;
+	// code validation function to check string for both environment name and word
+	parse_env = parse_env_var(env_str);
+	if (error)
+	{
+		free_envar((void *)parse_env);
+		return;
+	}
+	found = found_env(env, NULL, parse_env->name);
+	if (!found)
+		ft_lstadd_back(env, ft_lstnew((void *)parse_env));
+	else
+	{
+		free_envar(found->content);
+		found->content = parse_env;
+		return;
+	}
+}
+
+void unset_command(t_list **env, char *env_str)
+{
+	t_envar *parse_env;
+	t_list *found;
+	t_list *prev;
+
+	prev = NULL;
+	parse_env = parse_env_var(env_str);
+	if (error)
+		;
+	else
+	{
+		found = found_env(env, &prev, parse_env->name);
+		if (found)
+		{
+			prev->next = found->next;
+			ft_lstdelone(found, free_envar);
+		}
+	}
+	free_envar(parse_env);
 }
 
 int main(int argc, char *argv[], char *envp[])
@@ -138,7 +271,13 @@ int main(int argc, char *argv[], char *envp[])
 	t_list *env;
 
 	env = initialise_env(envp);
-	env_command(env);
+
+	unset_command(&env, "XDG_DATA_DIRS");
+	unset_command(&env, "WSL_INTEROP");
+	unset_command(&env, "WSL_DISTRO_NAME");
+	// export_add(&env, "WSL_DISTRO_NAME=lsajfd");
+	// env_command(env);
+	export_command(env);
 
 	// to free -> include towards end of main
 	ft_lstclear(&env, free_envar);
