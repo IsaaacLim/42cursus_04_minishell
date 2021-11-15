41e2d7568e5fc4b0514feece5a865eac3a71e771
isaaclim <isaaclimjj@yahoo.com>
2021-11-15 17:49:17 +0800

    added waitpid libraries

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,31 +1,34 @@
 #ifndef MINISHELL_H
-# define MINISHELL_H
+#define MINISHELL_H
 
-# include "libft.h"
+#include "libft.h"
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <readline/readline.h>
-# include <readline/history.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-typedef struct s_subprocess {
-	pid_t	pid;
-	int		stdin;
-	int		stdout;
-}	t_subprocess;
+typedef struct s_subprocess
+{
+	pid_t pid;
+	int stdin;
+	int stdout;
+} t_subprocess;
 
 //error.c
-void	ft_error(char *message);
+void ft_error(char *message);
 
 //fork.c
-void	ft_fork(char *argv[], t_subprocess *p);
+void ft_fork(char *argv[], t_subprocess *p);
 
 //lst_utils.c
-void	ft_lstdel(void *content);
-void	ft_putlst(t_list *lst);
+void ft_lstdel(void *content);
+void ft_putlst(t_list *lst);
 
 //readline.c
-void	ft_readline();
+void ft_readline();
 
 #endif
