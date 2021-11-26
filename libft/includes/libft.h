ddaec45375902c2e84278bb62e8febdc51ff5492
isaaclim <isaaclimjj@yahoo.com>
2021-11-26 17:20:00 +0800

    added ft_lstsize to coonvert env list to arr

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -17,6 +17,7 @@ void	ft_lstclear(t_list **lst, void(*del)(void *));
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 t_list	*ft_lstlast(t_list *lst);
 t_list	*ft_lstnew(void *content);
+int		ft_lstsize(t_list *lst);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 char	**ft_split(char const *s, char c);
 char	*ft_strdup(const char *s1);
