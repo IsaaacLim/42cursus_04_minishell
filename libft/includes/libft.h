06ad8fd17be2fda19b5f04474cb9eb02736b3126
isaaclim <isaaclimjj@yahoo.com>
2021-11-29 18:22:46 +0800

    updated libft

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -10,8 +10,10 @@ typedef struct s_list
 	struct s_list	*next;
 }	t_list;
 
+int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t size);
+int		ft_isdigit(int c);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstclear(t_list **lst, void(*del)(void *));
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
@@ -19,6 +21,8 @@ t_list	*ft_lstlast(t_list *lst);
 t_list	*ft_lstnew(void *content);
 int		ft_lstsize(t_list *lst);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putstr_fd(char *s, int fd);
 char	**ft_split(char const *s, char c);
 char	*ft_strdup(const char *s1);
 char	*ft_strjoin(char const *s1, char const *s2);
