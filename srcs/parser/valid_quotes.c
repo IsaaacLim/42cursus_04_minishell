33abe657b79f9e86f1737e524fce914c902190d3
jiaaren <jiaren.kjr@gmail.com>
2021-11-30 13:00:18 +0800

    to implement parser without quote type array

--- srcs/parser/valid_quotes.c
+++ srcs/parser/valid_quotes.c
@@ -26,11 +26,6 @@
 // 	return;
 // }
 
-/*
-	TO DO
-	0 replace strlen and calloc with libft functions
-*/
-
 // This assumes that str and track would have the same length
 bool valid_quotes(char *str, char *track, char to_rep, char rep_to)
 {
@@ -184,7 +179,7 @@ t_envar *found_null_term_env(t_list **env, char *ptr, int env_len)
 
 	Returns -1 if no changes are required to be made
 */
-int return_env_len(char *str, t_list **env)
+int return_env_len2(char *str, t_list **env)
 {
 	int tot_len;
 	int env_len;
@@ -224,7 +219,7 @@ int return_env_len(char *str, t_list **env)
 	-> pass in t_cmd
 	-> splits out t_cmd with updated string
 */
-char *update_with_env(char *str, t_list **env, int tot_len)
+char *update_with_env2(char *str, t_list **env, int tot_len)
 {
 	char *tmp;
 	int i;
@@ -258,14 +253,120 @@ char *update_with_env(char *str, t_list **env, int tot_len)
 	return (tmp);
 }
 
+char *check_update_env2(char *str, t_list **env)
+{
+	int len;
+
+	len = return_env_len2(str, env);
+	if (len != -1)
+		return (update_with_env2(str, env, len));
+	return (ft_strcpy(str));
+}
+
+int return_env_len(char *str, t_list **env)
+{
+	int tot_len;
+	int env_len;
+	char *ptr;
+	t_envar *envar;
+	bool to_update;
+	char quote;
+
+	quote = 0;
+	tot_len = 0;
+	while (*str)
+	{
+		if (!quote && (*str == '\'' || *str == '\"'))
+			quote = *str;
+		else if (*str == quote)
+			quote = 0;
+		else if (*str == '$' && *(str + 1) != ' ' && *(str + 1) != '$' && quote != '\'' && *(str + 1))
+		{
+			// need this to return whether to malloc a new string or not
+			ptr = str + 1;
+			env_len = 0;
+			// check to make sure that side by side $ works as well...
+			while (ptr[env_len] && ptr[env_len] != ' ' && ptr[env_len] != '$' && ptr[env_len] != quote && ptr[env_len])
+				env_len++;
+			envar = found_null_term_env(env, ptr, env_len);
+			if (envar)
+				tot_len += envar->word_len;
+			str += (env_len - 1);
+		}
+		else
+			tot_len++;
+		str++;
+	}
+	return (tot_len);
+}
+
+char *update_with_env(char *str, t_list **env, int tot_len)
+{
+	char *tmp;
+	int i;
+	int env_len;
+	char *ptr;
+	t_envar *envar;
+	char quote;
+
+	quote = 0;
+	tmp = malloc(sizeof(char) * (tot_len + 1));
+	i = 0;
+	while (*str)
+	{
+		if (!quote && (*str == '\'' || *str == '\"'))
+			quote = *str;
+		else if (*str == quote)
+			quote = 0;
+		else if (*str == '$' && *(str + 1) != ' ' && *(str + 1) != '$' && quote != '\'' && *(str + 1))
+		{
+			ptr = str + 1;
+			env_len = 0;
+			while (ptr[env_len] && ptr[env_len] != ' ' && ptr[env_len] != '$' && ptr[env_len] != quote && ptr[env_len])
+				env_len++;
+			envar = found_null_term_env(env, ptr, env_len);
+			if (envar)
+			{
+				ft_memcpy(&tmp[i], envar->word, envar->word_len);
+				i += envar->word_len;
+			}
+			str += env_len;
+		}
+		else
+			tmp[i++] = *str;
+		str++;
+	}
+	tmp[tot_len] = '\0';
+	return (tmp);
+}
+
 char *check_update_env(char *str, t_list **env)
 {
 	int len;
 
 	len = return_env_len(str, env);
+	return (update_with_env(str, env, len));
+}
+
+int main5(int argc, char *argv[], char *envp[])
+{
+	t_list *env = initialise_env(envp);
+
+	char str[] = "\"$WSL_DISTRO_NAME\"'$WSL_DISTRO_NAME' ";
+	char *tmp;
+
+	int len;
+
+	len = return_env_len2(str, &env);
 	if (len != -1)
-		return (update_with_env(str, env, len));
-	return (ft_strcpy(str));
+		tmp = update_with_env2(str, &env, len);
+	else
+		tmp = ft_strcpy(str);
+	printf("%i\n", len);
+	printf("%s\n", tmp);
+	free(tmp);
+	ft_lstclear(&env, free_envar);
+	return (0);
 }
 
 /*
@@ -304,20 +405,12 @@ int main2(void)
 
 int main3()
 {
-	// char str[] = "'hey' Hi \"  world\" ' asdf asdf   sadf sad f sda f sad f'";
 	char str[] = "\"export\"";
 	char str2[] = "export";
 	char **str_arr = ft_split_enhanced(str, ' ', NULL);
 	char **str_arr2 = ft_split(str, ' ');
 
 	printf("%i\n", ft_strncmp(str_arr[0], str_arr2[0], INT_MAX));
-	// int i = 0;
-	// while (str_arr[0][i] && str_arr2[0][i])
-	// {
-	// 	printf("%i %i %c %c\n", str_arr[0][i], str_arr[0][i], str_arr2[0][i], str_arr2[0][i]);
-	// 	i++;
-	// }
-	// printf("%i %i %c %c\n", str_arr[0][i], str_arr[0][i], str_arr2[0][i], str_arr2[0][i]);
 	if (str_arr2)
 	{
 		char **tmp = str_arr2;
@@ -345,5 +438,5 @@ int main4(int argc, char *argv[], char *envp[])
 
 // int main(int argc, char *argv[], char *envp[])
 // {
-// 	main3(argc, argv, envp);
+// 	main5(argc, argv, envp);
 // }
