e671d92cd6368ddd7113fefd74547244aca435e6
jiaaren <jiaren.kjr@gmail.com>
2021-11-30 10:51:00 +0800

    Norminette errors tidied up for parser.c and parser_utils.c

--- includes/parser.h
+++ includes/parser.h
@@ -11,53 +11,53 @@
 /* ************************************************************************** */
 
 #ifndef PARSER_H
-#define PARSER_H
+# define PARSER_H
 
-#include "libft.h"
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <limits.h>
+# include "libft.h"
+# include <stdio.h>
+# include <stdbool.h>
+# include <stdlib.h>
+# include <limits.h>
 
-typedef enum
+typedef enum e_redirection
 {
-    none,
-    in,
-    in_heredoc,
-    out,
-    out_append
-} e_redirection;
+	none,
+	in,
+	in_heredoc,
+	out,
+	out_append
+}			t_redirection;
 
 // Struct for simple command
 typedef struct s_cmd
 {
-    char **args;
-    int input;
-    int output;
-    char *infile;
-    char *outfile;
-    char *quote_type;
-} t_cmd;
+	char	**args;
+	int		input;
+	int		output;
+	char	*infile;
+	char	*outfile;
+}				t_cmd;
 
 // Struct to store array of simple commands
 // and length of array
 typedef struct s_commands
 {
-    t_cmd *commands;
-    int len;
-    char *quote_type;
-} t_commands;
+	t_cmd	*commands;
+	int		len;
+	char	*quote_type;
+}			t_commands;
 
 // Utility functions to be used in parser
-int num_pipes(char **str_arr);
-int is_redirection(char *str, t_cmd *cmd, char *next);
-bool valid_redirection(char **str_arr);
-bool valid_pipe(char **str_arr);
+int		num_pipes(char **str_arr);
+int		is_redirection(char *str, t_cmd *cmd, char *next);
+bool	valid_redirection(char **str_arr);
+bool	valid_pipe(char **str_arr);
 
-// To update with environment variable
-char *check_update_env(char *str, t_list **env);
+// To update str with environment variable
+char	*check_update_env(char *str, t_list **env);
 
-// Enhanced ft_split to consider validity of quotes and splitting according to quotes
-char **ft_split_enhanced(char *str, char c, char **quote_type);
+// Enhanced ft_split to consider validity of quotes 
+// and splitting according to quotes
+char	**ft_split_enhanced(char *str, char c, char **quote_type);
 
 #endif
